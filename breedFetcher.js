242dcda7f2f68d1c8e9d29fb63deab2bc6bb9c03
Katie <katie.herda2@gmail.com>
2021-09-23 20:02:38 +0000

    refactored code to take in a call back. Moced process.argv info and call back to a new index.js file.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,32 +1,46 @@
 const request = require('request');
-
-const args = process.argv.slice(2); //command line arguments
-
+//https://api.thecatapi.com/v1/breeds/search?q=
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-const breedSearch = args[0];
 
-request(`${URL}${breedSearch}`, (error, response, body) => {
-  //edge case: breen not found
-  if (body === '[]') {
-    console.log('Breed not found. Try again.');
-    return;
-  }
-  //edge case: request failed - ie. broken URL
-  if (error) {
-    console.error(error);
-    return;
-  }
+const fetchBreedDescription = (breedName, callBack) => {
 
-  console.log('error:', error);
-  console.log('statusCode:', response && response.statusCode); //=>200
-  //console.log('body:', body); //=>difficult to read as string
-  //console.log(typeof body); //=>string
+  //response is just a placeholder, not needed.
+  request(`${URL}${breedName}`, (error, response, body) => {
+    //edge case: breed not found
+    if (body === '[]') {
+      callBack(null, 'Breed not found. Try again.');
+      return;
+    }
+    //edge case: request failed - ie. broken URL
+    if (error) {
+      callBack(error, null);
+      return;
+    }
+  
+    //console.log('error:', error);
+    //console.log('statusCode:', response && response.statusCode); //=>200
+    //console.log('body:', body); //=>difficult to read as string
+    //console.log(typeof body); //=>string
+  
+    const data = JSON.parse(body); //convert JSON string into object
+    //console.log(data); //=> data in form of object
+    //console.log(typeof data); //=>object
+    
+    const breedInfo = data[0];
+    // callBack(null, breedInfo.description);
+    
+    if (breedInfo) {
+      callBack(null, breedInfo.description);
+    } else {
+      callBack('Breed Not Found');
+    }
+    
+    //Access the first entry in the data array and print description.
+    //console.log("data[0] : ", data[0].weight);
+  });
+};
 
-  const data = JSON.parse(body); //convert JSON string into object
-  console.log(data); //=> data in form of object
-  console.log(typeof data); //=>object
+module.exports = { fetchBreedDescription };
 
-  //Access the first entry in the data array and print description.
-  //console.log("data[0] : ", data[0].weight);
-});
+
 
---
+++ index.js
@@ -0,0 +1,14 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+
+const args = process.argv.slice(2); //command line arguments
+const breedName = args[0];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+ 
